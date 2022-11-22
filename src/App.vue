be7c153407afd5a3b50b8db1b39c2886d73105bb
NikiRufolo <nikirufolo@gmail.com>
2022-11-22 16:11:40 +0100

    start terminal vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <appTitle /> 
+    <!-- // in questo punto del codice di App Vue stampami il componente-->
+  </div>
+</template>
+
+<script>
+// ma per far si che funzioni devo importarmelo in import
+import appTitle from './components/appTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appTitle
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/appTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <!-- nostro codice -->
+        <h2>Ciao Mondo</h2>
+    </div>
+</template>
+<script>
+
+export default { //faccio un import in un altro file di questo file
+    name: 'appTitle', //nome componente
+} 
+</script>
